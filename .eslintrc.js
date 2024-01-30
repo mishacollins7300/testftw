5b1f158b8f9816e07e3270b7fb1fc743138b9f7b
Эмилия <e.muraveva@ritg.ru>
2024-01-30 21:37:00 +0400

    Пуш проекта

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,13 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/airbnb',
-    '@vue/typescript/recommended',
-  ],
+  extends: ['plugin:vue/vue3-essential', '@vue/airbnb', '@vue/typescript/recommended', 'prettier'],
   parserOptions: {
     ecmaVersion: 2020,
   },
+  plugins: ['vue', 'prettier'],
   rules: {
+    'prettier/prettier': 'error',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
